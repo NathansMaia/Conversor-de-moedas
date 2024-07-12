a369b6b1237df58520aa6b614ef775d3f4e8db7c
NathansMaia <nathanaeldasilvamaia@gmail.com>
2024-07-12 17:14:47 -0300

    em processo

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form');
+form.addEventListener('submit')
+
+const inputValue = document.getElementById('value-real')
+const selectedCurrency = document.getElementById('currency')
+const result = document.getElementById('result')
+
+function handleSubmit(e) {
+    alert('aloo')
+}
